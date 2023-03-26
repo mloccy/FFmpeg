8d97c441cfb55d36d7293e9b19282888d9b183c1
Colm Vize <colm.vize@unity3d.com>
2023-03-26 15:17:19 +0100

    wip

--- cmake/try_compile/stdatomic.c
+++ cmake/try_compile/stdatomic.c
@@ -2,7 +2,13 @@
 
 int main()
 {
+#ifdef _MSC_VER
+    _Atomic int foo, bar;
+    atomic_init(&bar, -1);
+#else
   atomic_int foo, bar = ATOMIC_VAR_INIT(-1);
+  #endif
+  
   atomic_store(&foo, 0);
   foo += bar;
 }
